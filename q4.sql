90c7be2929f68ff7ab4b3c69558f7d50bbcb858e
Ekansh-Patidar <84856232+Ekansh-Patidar@users.noreply.github.com>
2025-08-15 20:00:08 +0530

    Add files via upload

---
+++ q4.sql
@@ -0,0 +1,29 @@
+DELIMITER $$
+
+CREATE PROCEDURE SendWatchTimeReport()
+BEGIN
+    DECLARE done INT DEFAULT 0;
+    DECLARE subId INT;
+
+    DECLARE cur CURSOR FOR
+        SELECT DISTINCT SubscriberID
+        FROM WatchHistory;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN cur;
+
+    read_loop: LOOP
+        FETCH cur INTO subId;
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        CALL GetWatchHistoryBySubscriber(subId);
+
+    END LOOP;
+
+    CLOSE cur;
+END$$
+
+DELIMITER ;
